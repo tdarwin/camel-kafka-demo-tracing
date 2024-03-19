258385cfba7b9302ad148ea3bc0b976e741a907e
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-03-19 16:58:41 +0000

    inject otel sdk as bean

--- camel-kafka-demo-producer/src/main/java/com/iaa/camelkafkademoproducer/CamelRouteBuilder.java
+++ camel-kafka-demo-producer/src/main/java/com/iaa/camelkafkademoproducer/CamelRouteBuilder.java
@@ -1,20 +1,27 @@
 package com.iaa.camelkafkademoproducer;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 
 @Component
 public class CamelRouteBuilder extends RouteBuilder {
+  private final OpenTelemetry openTelemetry;
+
+  public CamelRouteBuilder(@Autowired OpenTelemetry openTelemetry) {
+    this.openTelemetry = openTelemetry;
+  }
+
   @Override
   public void configure() throws Exception {
     // from("timer:foo").to("log:bar");
     from("kafka:pageviews?brokers=localhost:9092")
         .process(expression -> {
-          Tracer tracer = GlobalOpenTelemetry.getTracer("camel-consumer-tracer");
+          Tracer tracer = openTelemetry.getTracer("camel-producer-tracer");
           Span mapperSpan = tracer.spanBuilder("producer-mapper").startSpan();
           // Custom processing logic
           String body = expression.getIn().getBody(String.class);
